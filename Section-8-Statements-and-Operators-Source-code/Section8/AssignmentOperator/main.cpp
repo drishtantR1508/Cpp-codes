819cadb29d68bfc830ebd9b492c739cea0b3b4db
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2020-12-26 09:26:58 +0530

    C++ Codes

---
+++ Section-8-Statements-and-Operators-Source-code/Section8/AssignmentOperator/main.cpp
@@ -0,0 +1,23 @@
+// Section 8
+// Assignment operator (=)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 {10};
+    int num2 {20};
+    
+    int num = 100;
+    
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    cout <<"num is "<<num<<endl;
+    cout<<"Enter the new value for num1 : ";
+    cin >> num1;
+    cout << "num1 is now "<<num1<<endl;
+    return 0;
+}
+
